ec97fa302c4b80ab95b16b44762d27bde44e88cd
SleentO <support@sleento.com>
2022-03-04 03:05:25 +0100

    Add Docs Long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, Extension
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
   name = 'letesend',         # How you named your package folder (MyLib)
   packages = ['letesend'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Letesend sdk for sending and monitoring SMS',   # Give a short description about your library
+  long_description=long_description,
+   long_description_content_type="text/markdown",
   author = 'Letesend',                   # Type in your name
   author_email = 'support@letesend.com',      # Type in your E-Mail
   url = 'https://github.com/letesend/python',   # Provide either the link to your github or to your website
